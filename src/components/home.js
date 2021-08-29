1efc712eb19c0ce3a95748284d71972ff2b64785
Hashim Hashimli <31937184+Hashim1995@users.noreply.github.com>
2021-08-29 18:22:17 +0400

    pull request

--- src/components/home.js
+++ src/components/home.js
@@ -1,12 +1,19 @@
 import React from "react";
 import Services from "./subComponents/Services";
 import CarouselSlider from "./subComponents/CarouselSlider";
+import { Row, Col } from "antd";
 
 const Home = () => {
   return (
     <div>
       <CarouselSlider />
-      <Services />
+      <Row>
+        <Col span={24}>
+          <Col span={16} offset={4}>
+            <Services />
+          </Col>
+        </Col>
+      </Row>
       Main page
     </div>
   );
--- src/components/subComponents/Services.js
+++ src/components/subComponents/Services.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { Divider } from "antd";
 
 const Services = () => {
   return (
     <div>
       <h4>MAIN SERVICES</h4>
+      <Divider className="mainServiceTitle" dashed={true}>
+        MAIN SERVICES
+      </Divider>
     </div>
   );
 };
