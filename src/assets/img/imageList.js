0878f482d6e2d8734f7200930ee5b96545572683
Hashim Hashimli <31937184+Hashim1995@users.noreply.github.com>
2021-08-29 21:06:46 +0400

    pull reuqest

--- src/assets/img/imageList.js
+++ src/assets/img/imageList.js
@@ -10,9 +10,14 @@ import hook from "./hook.svg";
 import pipeline from "./pipeline.svg";
 import repairman from "./repairman.svg";
 import energy from "./smart-energy.svg";
+import workerOne from "./workers1.jpg";
+import workerTwo from "./workers2.jpg";
+import workerThree from "./workers3.jpg";
+import workerFour from "./workers4.jpg";
 
 const CarouselImg = [car1, car2, car3, car4, car5];
 const ServicesIcons = [drill, home, hook, pipeline, repairman, energy];
 const logoImg = logo;
+const workersImg = [workerOne, workerTwo, workerThree, workerFour];
 
-export { CarouselImg, logoImg, ServicesIcons };
+export { CarouselImg, logoImg, ServicesIcons, workersImg };
--- src/components/subComponents/WhyUs.js
+++ src/components/subComponents/WhyUs.js
@@ -1,12 +1,93 @@
 import React from "react";
-import { Divider } from "antd";
+import { Divider, Collapse } from "antd";
+import { Image } from "antd";
+import { workersImg } from "../../assets/img/imageList.js";
 
 const WhyUs = () => {
+  const { Panel } = Collapse;
   return (
     <div>
       <Divider className="WhyUsTitle" dashed={true}>
         WHY CHOOSE US?
       </Divider>
+      <div>
+        <div className="WhyUsWrap">
+          <Collapse
+            accordion
+            className="WhyUsCollapse"
+            defaultActiveKey={["1"]}
+          >
+            <Panel
+              className="WhyUsPanel"
+              header="We have 30 plus years in the building industry"
+              key="1"
+            >
+              <p>
+                {" "}
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. In a
+                metus pellentesque, scelerisque ex sed, volutpat nisi. Curabitur
+                tortor mi, eleifend ornare lobortis non. Nulla porta purus quis
+                iaculis ultrices. Proin aliquam sem at nibh hendrerit sagittis.
+                Nullam ornare odio eu lacus tincidunt malesuada. Sed eu
+                vestibulum elit. Curabitur tortor mi, eleifend ornare.
+              </p>
+            </Panel>
+            <Panel
+              className="WhyUsPanel"
+              header="Quality construction continues after the project"
+              key="2"
+            >
+              <p>
+                {" "}
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. In a
+                metus pellentesque, scelerisque ex sed, volutpat nisi. Curabitur
+                tortor mi, eleifend ornare lobortis non. Nulla porta purus quis
+                iaculis ultrices. Proin aliquam sem at nibh hendrerit sagittis.
+                Nullam ornare odio eu lacus tincidunt malesuada. Sed eu
+                vestibulum elit. Curabitur tortor mi, eleifend ornare.
+              </p>
+            </Panel>
+            <Panel
+              className="WhyUsPanel"
+              header="We use technology to do the job more quickly"
+              key="3"
+            >
+              <p>
+                {" "}
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. In a
+                metus pellentesque, scelerisque ex sed, volutpat nisi. Curabitur
+                tortor mi, eleifend ornare lobortis non. Nulla porta purus quis
+                iaculis ultrices. Proin aliquam sem at nibh hendrerit sagittis.
+                Nullam ornare odio eu lacus tincidunt malesuada. Sed eu
+                vestibulum elit. Curabitur tortor mi, eleifend ornare.
+              </p>
+            </Panel>
+            <Panel
+              className="WhyUsPanel"
+              header="Employees are continually trained on safety issues"
+              key="4"
+            >
+              <p>
+                {" "}
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. In a
+                metus pellentesque, scelerisque ex sed, volutpat nisi. Curabitur
+                tortor mi, eleifend ornare lobortis non. Nulla porta purus quis
+                iaculis ultrices. Proin aliquam sem at nibh hendrerit sagittis.
+                Nullam ornare odio eu lacus tincidunt malesuada. Sed eu
+                vestibulum elit. Curabitur tortor mi, eleifend ornare.
+              </p>
+            </Panel>
+          </Collapse>
+          <div className="WhyUsGallery">
+            <Image.PreviewGroup>
+              <Image width={"100%"} src={workersImg[0]} />
+              <Image width={"33%"} src={workersImg[1]} />
+              <Image width={"33%"} src={workersImg[2]} />
+              <Image width={"33%"} src={workersImg[3]} />
+            </Image.PreviewGroup>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
